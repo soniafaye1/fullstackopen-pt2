b4712fe0b4211a69eb93a1fca7581305bf78b339
sonia <soniafaye1@gmail.com>
2024-10-19 22:55:55 -0400

    started phonebook pt 2

--- src/PhoneBook.jsx
+++ src/PhoneBook.jsx
@@ -2,23 +2,43 @@ import { useState } from 'react'
 
 const PhoneBook = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    {name: 'Arto Hellas' }
   ]) 
   const [newName, setNewName] = useState('')
+  const [nameExists, setNameExists] = useState(true)
+
+  const handleNewName = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handleNewPerson = (event) => {
+    event.preventDefault()
+    const newPerson = {
+        name: newName
+    }
+
+    setPersons(persons.concat(newPerson))
+    setNewName('')
+  }
+
+  const existsAlready = nameExists ? persons.filter(person => persons.includes(person.name)) : newName;
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={handleNewPerson}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNewName} />
         </div>
+        {!existsAlready ? alert(`${newName} already exists`) : "" }
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+        <ul>
+            {persons.map(person => <li key={person.name}>{person.name}</li> )}
+        </ul>
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import Notes from './Notes.jsx'
+import PhoneBook from './PhoneBook.jsx'
 
 const startingNotes = [
   {
@@ -24,6 +25,7 @@ const startingNotes = [
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <App /> */}
-    <Notes startingNotes={startingNotes}/>
+    {/* <Notes startingNotes={startingNotes}/> */}
+    <PhoneBook />
   </StrictMode>,
 )
